cd7efa66a2082944ef04c6c00f11ef6209e23b80
shabib <shabib@gmail.com>
2024-02-18 02:01:02 +0430

    Adding files to git repository

---
+++ src/Pages/Burrons.js
@@ -0,0 +1,12 @@
+const Buttons = () => {
+    return (
+        <>
+            <div className="entery">
+                <i className=""></i>
+                <button>{props.form}</button>
+            </div>
+        </>
+    )
+}
+
+export default Buttons;
