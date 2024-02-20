9e2561e696f85afde265cdbe8f32b080eb5c39db
shabib <shabib@gmail.com>
2024-02-20 16:18:11 +0430

    adding leave form

--- src/Pages/CarReequestForm/CarRequestForm.js
+++ src/Pages/CarReequestForm/CarRequestForm.js
@@ -1,7 +1,5 @@
-
 import React, { useState } from 'react';
 import "bootstrap/dist/css/bootstrap.min.css"
-
 import "./CarRequestForm.css"
 const CarRequestForm = () => {
     const [formData, setFormData] = useState({
