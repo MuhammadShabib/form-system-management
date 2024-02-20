8e3497740133173a888fcf28be7365a68d3c6be4
shabib <shabib@gmail.com>
2024-02-21 02:50:21 +0430

    fixing Car Requesting Form Styles

--- src/Pages/CarReequestForm/CarRequestForm.js
+++ src/Pages/CarReequestForm/CarRequestForm.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import "bootstrap/dist/css/bootstrap.min.css"
 import "./CarRequestForm.css"
+import PrintRestHome from '../../Components/Button/PrintResetHome';
 const CarRequestForm = () => {
     const [formData, setFormData] = useState({
         personName: '',
@@ -11,54 +12,59 @@ const CarRequestForm = () => {
         purpose: '',
     });
     return (
-        <div className="car-request-form mt-5">
+        <div className="car-request-form mt-5 container">
             <div className="header">
                 {/* <img src="../../1.jpg" alt="" /> */}
                 <h2 className='text-center'>Car Request Form</h2>
             </div>
             <form className='m-4'>
-                <div className="mb-3 input"> <label class="form-label">Name:</label>
-                    <input
-                        class="form-control"
-                        placeholder='Name of Person Requesting Car'
-                        type="text"
-                        value={formData.personName}
-                        onChange={(e) => setFormData({ ...formData, personName: e.target.value })}
-                    />
+                <div className='beside'>
+                    <div className="mb-3 ">
+                        <label class="form-label">Name:</label>
+                        <input
+                            class="form-control"
+                            placeholder='Name of Person Requesting Car'
+                            type="text"
+                            value={formData.personName}
+                            onChange={(e) => setFormData({ ...formData, personName: e.target.value })}
+                        />
+                    </div>
+
+                    <div className="mb-3">
+                        <label class="form-label">Branch Name:</label>
+                        <input
+                            class="form-control"
+                            placeholder='Branch Name'
+                            type="text"
+                            value={formData.branchName}
+                            onChange={(e) => setFormData({ ...formData, branchName: e.target.value })}
+                        />
+                    </div>
                 </div>
 
-                <div className="mb-3 input">
-                    <label class="form-label">Branch Name:</label>
-                    <input
-                        class="form-control"
-                        placeholder='Branch Name'
-                        type="text"
-                        value={formData.branchName}
-                        onChange={(e) => setFormData({ ...formData, branchName: e.target.value })}
-                    />
+                <div className='beside'>
+                    <div className="mb-3 ">
+                        <label class="form-label">Date of Requesting:</label>
+                        <input
+                            class="form-control"
+                            type="date"
+                            value={formData.requestDate}
+                            onChange={(e) => setFormData({ ...formData, requestDate: e.target.value })}
+                        />
+                    </div>
+
+                    <div className="mb-3 ">
+                        <label class="form-label">Time of Requesting:</label>
+                        <input
+                            class="form-control"
+                            type="time"
+                            value={formData.requestTime}
+                            onChange={(e) => setFormData({ ...formData, requestTime: e.target.value })}
+                        />
+                    </div>
                 </div>
 
-                <div className="mb-3 input">
-                    <label class="form-label">Date of Requesting:</label>
-                    <input
-                        class="form-control"
-                        type="date"
-                        value={formData.requestDate}
-                        onChange={(e) => setFormData({ ...formData, requestDate: e.target.value })}
-                    />
-                </div>
-
-                <div className="mb-3 input">
-                    <label class="form-label">Time of Requesting:</label>
-                    <input
-                        class="form-control"
-                        type="time"
-                        value={formData.requestTime}
-                        onChange={(e) => setFormData({ ...formData, requestTime: e.target.value })}
-                    />
-                </div>
-
-                <div className="mb-3 input">
+                <div className="mb-3 ">
                     <label class="form-label">Destination:</label>
                     <input
                         class="form-control"
@@ -68,7 +74,7 @@ const CarRequestForm = () => {
                     />
 
                 </div>
-                <div className="mb-3 input">
+                <div className="mb-3 ">
                     <label class="form-label">Purpose of Travel:</label>
                     <input
                         class="form-control"
@@ -84,7 +90,7 @@ const CarRequestForm = () => {
 
                 </div>
 
-                <button className='btn btn-primary mt-5' onClick={window.print}>Print</button>
+                <PrintRestHome />
             </form>
         </div>
     );
