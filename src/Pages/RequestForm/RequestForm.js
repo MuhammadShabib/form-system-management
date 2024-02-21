8bfeb35d438d9c5c70eb58730d643ba9ac291d66
shabib <shabib@gmail.com>
2024-02-22 02:14:41 +0430

    Adding Request form

--- src/Pages/RequestForm/RequestForm.js
+++ src/Pages/RequestForm/RequestForm.js
@@ -1,4 +1,9 @@
 import React, { useState } from "react";
+import PrintResetHome from "../../Components/Button/PrintResetHome"
+import "../Pages.css"
+import "./RequestForm.css"
+import "bootstrap/dist/css/bootstrap.min.css"
+import RequestTable from "../../Components/TableOfRequest/TableOfRequest"
 const RequestForm = () => {
     const [dateOfRequest, setDateOfRequest] = useState('');
     const [dateRequired, setDateRequired] = useState('');
@@ -23,110 +28,168 @@ const RequestForm = () => {
     const [dateOfCheckedBy, setDateOfCheckedBy] = useState('');
     const [dateOfApprovedBy, setDateOfApprovedBy] = useState('');
     return (
-        <div className="purchase-form">
-            <h1>WASSA Purchase Requisition Form</h1>
-            <div>
-                <label>Date of Request:</label>
-                <input type="date" value={dateOfRequest} onChange={(e) => setDateOfRequest(e.target.value)} />
+        <div className="container">
+            <form>
+                <h1>WASSA Purchase Requisition Form</h1>
+                <div className="beside">
+                    <div>
+                        <label className="form-label">Date of Request:</label>
+                        <input className="form-control" type="date" value={dateOfRequest} onChange={(e) => setDateOfRequest(e.target.value)} />
+                    </div>
+                    <div>
+                        <label className="form-label">Date Required:</label>
+                        <input className="form-control" type="date" value={dateRequired} onChange={(e) => setDateRequired(e.target.value)} />
+                    </div>
+                </div>
+                <div className="beside">
+                    <div>
+                        <label className="form-label">Project/Department:</label>
+                        <input className="form-control" type="text" value={projectDepartment} onChange={(e) => setProjectDepartment(e.target.value)} />
+                    </div>
+                    <div>
+                        <label className="form-label">Budget Line:</label>
+                        <input className="form-control" type="text" value={budgetLine} onChange={(e) => setBudgetLine(e.target.value)} />
+                    </div>
+                </div>
+                <div>
+                    <div className="beside">
+                        <div>
+                            <label className="form-label">Location:</label>
+                            <input className="form-control" type="text" value={location} onChange={(e) => setLocation(e.target.value)} />
+                        </div>
+                        <div className="besidemore mt-4" >
+                            <label className="form-label">Payment Type:</label>
+                            <div  >
+                                <input type="radio" className="form-check-input me-2" name="payment" value={paymentType} onChange={handlePaymentChange} />
+                                <label className="form-label" htmlFor="cheque">Cheque</label>
+                            </div>
+                            <div>
+                                <input type="radio" className="form-check-input me-2" name="payment" value={paymentType} onChange={handlePaymentChange} />
+                                <label className="form-label" htmlFor="cash">Cash</label>
+                            </div>
+                            <div>
+                                <input type="radio" className="form-check-input me-2" name="payment" value={paymentType} onChange={handlePaymentChange} />
+                                <label className="form-label" htmlFor="transfer">Transfer</label>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <p>Complete the information and obtain approvals below. Forward the completed and approved form to the Purchasing Department.</p>
+                <p>Note: Forms that are not approved or incomplete will be returned to the requisitioner.</p>
+                {/* Table */}
+                {/* Table End */}
+                <RequestTable />
+                <p>The above items are required to be used for all classes in all centers</p>
+                {/* Third Part */}
+                <div>
+                    <div className="besidemore third">
+                        <div className="beside">
+                            <div>
+                                <label className="form-label">
+                                    Requested by:
+                                </label>
+                                <input className="form-control" type="text" value={requestedBy} onChange={(e) => setRequestedBy(e.target.value)} />
 
-                <label>Date Required:</label>
-                <input type="date" value={dateRequired} onChange={(e) => setDateRequired(e.target.value)} />
-            </div>
-            <div>
-                <label>Project/Department:</label>
-                <input type="text" value={projectDepartment} onChange={(e) => setProjectDepartment(e.target.value)} />
-                <label>Budget Line:</label>
-                <input type="text" value={budgetLine} onChange={(e) => setBudgetLine(e.target.value)} />
-            </div>
-            <div>
-                <label>Location:</label>
-                <input type="text" value={location} onChange={(e) => setLocation(e.target.value)} />
-                <div>
-                    <label>Payment Type:</label>
-                    <div>
-                        <input type="radio" name="payment" value={paymentType} onChange={handlePaymentChange} />
-                        <label htmlFor="cheque">Cheque</label>
+                            </div>
+                            <div>
+                                <label className="form-label">
+                                    Position:
+                                </label>
+                                <input className="form-control" type="text" value={position1} onChange={(e) => setPosition1(e.target.value)} />
+
+                            </div>
+                        </div>
+                        <div className="mt-5 ">Signature:</div>
                     </div>
-                    <div>
-                        <input type="radio" name="payment" value={paymentType} onChange={handlePaymentChange} />
-                        <label htmlFor="cash">Cash</label>
+                    <div className="besidemore third">
+                        <div className="beside">
+                            <div>
+                                <label className="form-label">
+                                    Authorized by:
+                                </label>
+                                <input className="form-control" type="text" value={authorizedBy} onChange={(e) => setAuthorizedBy(e.target.value)} />
+                            </div>
+                            <div>
+                                <label className="form-label">
+                                    Position:
+                                </label>
+                                <input className="form-control" type="text" value={position2} onChange={(e) => setPosition2(e.target.value)} />
+                            </div>
+                        </div>
+                        <div className="mt-5 ">Signature:</div>
                     </div>
+                </div>
+                <div className="besidemore ">
+                    <div className="on">
+                        <div>
+                            <label className="form-label">
+                                Verified by (Proc/Log): </label>
+                            <input className="form-control" type="text" value={verifiedBy} onChange={(e) => setVerifiedBy(e.target.value)} />
+
+                        </div>
+                        <div>
+                            <label className="form-label">
+                                Position:
+                            </label>
+                            <input className="form-control" type="text" value={position3} onChange={(e) => setPosition3(e.target.value)} />
+                        </div>
+                        <div>
+                            <label className="form-label">Date:</label>
+                            <input className="form-control" type="date" value={dateOfVerifiedBy} onChange={(e) => setDateOfVerifiedBy(e.target.value)} />
+                        </div>
+                    </div>
+                    <div className="on">
+                        <div>
+                            <label className="form-label">
+                                Checked by (Finance):
+                            </label>
+                            <input className="form-control" type="text" value={checkedBy} onChange={(e) => setCheckedBy(e.target.value)} />
+
+                        </div>
+                        <div>
+                            <label className="form-label">
+                                Position:
+                            </label>
+                            <input className="form-control" type="text" value={position4} onChange={(e) => setPosition4(e.target.value)} />
+
+                        </div>
+                        <label className="form-label">Date:</label>
+                        <input className="form-control" type="date" value={dateOfCheckedBy} onChange={(e) => setDateOfCheckedBy(e.target.value)} />
+                    </div>
+                    <div className="on">
+                        <div>
+                            <label className="form-label">
+                                Approved by:
+                            </label>
+                            <input className="form-control" type="text" value={approvedBy} onChange={(e) => setApprovedBy(e.target.value)} />
+
+                        </div>
+                        <div>
+                            <label className="form-label">
+                                Position:
+                            </label>
+                            <input className="form-control" type="text" value={position5} onChange={(e) => setPosition5(e.target.value)} />
+
+                        </div>
+                        <div>
+                            <label className="form-label">Date:</label>
+                            <input className="form-control" type="date" value={dateOfApprovedBy} onChange={(e) => setDateOfApprovedBy(e.target.value)} />
+                        </div>
+                    </div>
+
+                </div>
+                <div className="besidemore sign1">
                     <div>
-                        <input type="radio" name="payment" value={paymentType} onChange={handlePaymentChange} />
-                        <label htmlFor="transfer">Transfer</label>
+                        <div></div>
+                    </div>
+                    <div className="beside sign2 mt-3">
+                        <div>Signature:</div>
+                        <div>Signature:</div>
                     </div>
                 </div>
-            </div>
-            <p>Complete the information and obtain approvals below. Forward the completed and approved form to the Purchasing Department.</p>
-            <p>Note: Forms that are not approved or incomplete will be returned to the requisitioner.</p>
-            {/* Table */}
-            {/* Table End */}
-            <p>The above items are required to be used for all classes in all centers</p>
-            {/* Third Part */}
-            <div>
-                <div>
-                    <label>
-                        Requested by:
-                        <input type="text" value={requestedBy} onChange={(e) => setRequestedBy(e.target.value)} />
-                    </label>
-                    <label>
-                        Position:
-                        <input type="text" value={position1} onChange={(e) => setPosition1(e.target.value)} />
-                    </label>
-                    <div>Signature:</div>
-                </div>
-                <div>
-                    <label>
-                        Authorized by:
-                        <input type="text" value={authorizedBy} onChange={(e) => setAuthorizedBy(e.target.value)} />
-                    </label>
-                    <label>
-                        Position:
-                        <input type="text" value={position2} onChange={(e) => setPosition2(e.target.value)} />
-                    </label>
-                    <div>Signature:</div>
-                </div>
-            </div>
-            <div>
-                <div>
-                    <label>
-                        Verified by (Proc/Log):
-                        <input type="text" value={verifiedBy} onChange={(e) => setVerifiedBy(e.target.value)} />
-                    </label>
-                    <label>
-                        Position:
-                        <input type="text" value={position3} onChange={(e) => setPosition3(e.target.value)} />
-                    </label>
-                    <label>Date:</label>
-                    <input type="date" value={dateOfVerifiedBy} onChange={(e) => setDateOfVerifiedBy(e.target.value)} />
-                </div>
-                <div>
-                    <label>
-                        Checked by (Finance):
-                        <input type="text" value={checkedBy} onChange={(e) => setCheckedBy(e.target.value)} />
-                    </label>
-                    <label>
-                        Position:
-                        <input type="text" value={position4} onChange={(e) => setPosition4(e.target.value)} />
-                    </label>
-                    <label>Date:</label>
-                    <input type="date" value={dateOfCheckedBy} onChange={(e) => setDateOfCheckedBy(e.target.value)} />
-                </div>
-                <div>
-                    <label>
-                        Approved by:
-                        <input type="text" value={approvedBy} onChange={(e) => setApprovedBy(e.target.value)} />
-                    </label>
-                    <label>
-                        Position:
-                        <input type="text" value={position5} onChange={(e) => setPosition5(e.target.value)} />
-                    </label>
-                    <label>Date:</label>
-                    <input type="date" value={dateOfApprovedBy} onChange={(e) => setDateOfApprovedBy(e.target.value)} />
-                </div>
-                <div>Signature:</div>
-                <div>Signature:</div>
-            </div>
+                <PrintResetHome />
+            </form>
+
         </div>
     );
 };
