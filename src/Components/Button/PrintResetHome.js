87dd6f0d7b0e99040a25e5d3f59bb34db93a03fd
shabib <shabib@gmail.com>
2024-02-24 08:01:59 +0430

    adding functionality ti home button and reset buttont

--- src/Components/Button/PrintResetHome.js
+++ src/Components/Button/PrintResetHome.js
@@ -1,13 +1,37 @@
-import "./Button.css"
-import { Link } from "react-router-dom"
+import "./Button.css";
+import { Link } from "react-router-dom";
 
 const LinkButtons = () => {
     return (
-        <div className="bnts">
-            <button className='btn btn-primary me-3' onClick={window.print}> Print</button>
-            <Link className='ms-auto btn btn-outline-primary' to="/">Back To Home Page</Link>
-            <input type="reset" className='btn btn-primary ms-3' value=" Reset" />
-        </div>
-    )
-}
-export default LinkButtons
+        <>
+            <div className="bnts">
+                <button className="btn btn-success me-3" onClick={window.print}>
+                    Print
+                </button>
+                <Link
+                    className="ms-auto btn btn-outline-primary"
+
+                    onClick={(e) => {
+                        const ask = window.confirm("Do you want to go to home page");
+                        if (!ask) e.preventDefault();
+                    }}
+                    to="/" >
+                    Back To Home Page
+                </Link>
+                <button
+                    className="btn btn-danger ms-3"
+                    onClick={(e) => {
+                        const ask = window.confirm("Do you want to reset form")
+                        if (ask) document.querySelector("form").reset()
+                        else e.preventDefault()
+
+                    }}
+                >
+                    Reset
+                </button>
+            </div >
+        </>
+    );
+};
+
+export default LinkButtons;
